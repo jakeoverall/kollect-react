2354db74000bf7ba2accb3a2d2550663a9dc6bdb
Jake Overall <joverall@gmail.com>
2024-12-17 10:38:04 -0700

    data dump example

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  //                        vv React Hook
-  const [count, setCount] = useState(0)
-  //    v destructuring
-  const [x, setX] = useState(10)
 
-  // x[0] == number
-  // x[1] == setterFunction
+  const cards = [
+    { id: 0, name: 'Thorsten', image: '🦍' },
+    { id: 1, name: 'Georgie', image: '🐒' }
+  ]
 
-  function incrementX(){
-    setX(x + 1)
-    console.log(x)
-  }
-
-
-  // 🎗️ REMINDER ITS ALL FAKE EVERYTHING IS FAKE
-  // let div = {
-  //   className: 'card'
-  // }
-
-  const cardClass = "card"
 
   return (
     <>
-      <h1>Cool Stuff</h1>
-
-      <div className={cardClass}>
-        <button onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-        <div>
-          <button onClick={incrementX}>{x}</button>
-        </div>
-      </div>
-
+      {/* DATA DUMP EXAMPLE */}
+      <pre><code>{JSON.stringify(cards, null, 2)}</code></pre>
     </>
   )
 }
